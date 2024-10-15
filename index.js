95a488daf3d71ccb62673fc8b5bf1904a80523bc
pitchblendelegit <72884743+pitchblendelegit@users.noreply.github.com>
2024-10-15 13:35:29 +0700

    api routes fixed

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.post('/api/shorturl', function(req, res) {
   }
 });
 
-app.get("/api/short_url/:short_url", async(req, res)=>{
+app.get("/api/shorturl/:short_url", async(req, res)=>{
   const shorturl = req.params.short_url;
   const urlDoc = await urls.findOne({short_url: +shorturl});
   res.redirect(urlDoc.url);
